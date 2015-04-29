438c8e1236c61f22cbaabb47f87e2ce24bae9f0a
alegent <alegent@student.42.fr>
2015-04-29 09:55:41 +0200

    fuck

--- srcs/lsort.c
+++ srcs/lsort.c
@@ -6,7 +6,7 @@
 /*   By: alegent <alegent@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/04/22 14:34:45 by alegent           #+#    #+#             */
-/*   Updated: 2015/04/22 14:34:48 by alegent          ###   ########.fr       */
+/*   Updated: 2015/04/22 15:24:54 by alegent          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,10 @@ void					lsort(t_env *e)
 
 	tmp = e->a->begin;
 	if (tmp->data > tmp->next->data && tmp->next->data > tmp->next->next->data)
-		reverse_rotate(e, 'a');
-	else if (tmp->data > tmp->next->data
-			&& tmp->next->data < tmp->next->next->data
-			&& tmp->data > tmp->next->next->data)
-		rotate(e, 'a');
-	else if (tmp->data > tmp->next->data
-			&& tmp->next->data < tmp->next->next->data
-			&& tmp->data < tmp->next->next->data)
+	{
 		swap(e, 'a');
-	else if (tmp->data < tmp->next->data
-			&& tmp->next->data > tmp->next->next->data
-			&& tmp->data > tmp->next->next->data)
 		reverse_rotate(e, 'a');
+	}
 	else if (tmp->data < tmp->next->data
 			&& tmp->next->data > tmp->next->next->data
 			&& tmp->data < tmp->next->next->data)
